2e2630caab3ab3904c1783005faede7e644d08ae
luckydwipasb@students.amikom.ac.id <luckydwipasb@gmail.com>
2022-04-28 21:40:17 +0800

    inisialisasi project

---
+++ Uts/Dosen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uts
+{
+    public class Dosen
+    {
+        public void dosen()
+        {
+            Console.WriteLine("Dosen Pengampu   : Kamarudin, M.kom");
+            Console.WriteLine("ID               : 29876xxxxx");
+            Console.WriteLine("Mata kuliah      : Pemograman");
+            Console.WriteLine("Gender           : Laki-Laki");
+        }
+        public void attDosen(string nama, int nik)
+        {
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Nama Dosen   : " + nama);
+            Console.WriteLine("NIK Dosen    : " + nik);
+        }
+        public void addCourse(string course)
+        {
+            Console.WriteLine("Mataka Kuliah Tambahan   : " + course);
+        }
+    }
+}
---
+++ Uts/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uts
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dosen dosen = new Dosen();
+
+            //pemanggilan method
+            Console.WriteLine("================================================");
+            Console.WriteLine("Method Ke-1");
+            Console.WriteLine("================================================");
+            dosen.dosen();
+            Console.WriteLine("================================================");
+            Console.WriteLine("Method Ke-2");
+            Console.WriteLine("================================================");
+            Console.Write("Masukkan Nama    : ");
+            string nama;
+            nama = Console.ReadLine();
+            Console.Write("Masukkan NIK     : ");
+            int nik;
+            nik = int.Parse(Console.ReadLine());
+            dosen.attDosen(nama, nik);
+            Console.WriteLine("================================================");
+            Console.WriteLine("Method Ke-3");
+            Console.WriteLine("================================================");
+            Console.Write("Masukkan Mata Kuliah Tambahan : ");
+            string course;
+            course = Console.ReadLine();
+            Console.WriteLine("----------------------");
+            dosen.dosen();
+            dosen.addCourse(course);
+            Console.WriteLine("================================================");
+
+            Console.ReadKey();
+        }
+    }
+}
